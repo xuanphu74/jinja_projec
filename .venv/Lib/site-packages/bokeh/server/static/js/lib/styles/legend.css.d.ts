a513c2f3d0e45afb4846e9a78cc2d3749680992f
roxlet <73515613+shadow12702@users.noreply.github.com>
2025-03-17 14:28:56 +0700

    main

---
+++ .venv/Lib/site-packages/bokeh/server/static/js/lib/styles/legend.css.d.ts
@@ -0,0 +1,12 @@
+export const hidden: string
+export const title: string
+export const label: string
+export const grid: string
+export const vertical: string
+export const item: string
+export const styled: string
+export const active: string
+export const interactive: string
+export const glyph: string
+const __css__: string
+export default __css__
